f13baf2b3428a37dd300567999f27772bc088b3b
Unknown <dylan.gogeta@gmail.com>
2018-12-01 12:19:17 +0100

    Main
    Le corps du projets

---
+++ KtaPulte/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using KtaPulte.Class;
+
+
+namespace KtaPulte
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Catapulte KTA = new Catapulte();
+            KTA.Assemblage();
+            KTA.Tirer(45);
+            string pierre = "Une pierre de 5 Kg";
+            KTA.Tirer(pierre);
+            KTA.getLifeSpoon();
+            KTA.getLifeSpoon();
+            KTA.setDamageBody(6);
+            Console.ReadKey();
+        }
+    }
+}
