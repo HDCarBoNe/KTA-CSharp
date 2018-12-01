ca3a0b3f4d4de9a095be401a6068da669b9fbabc
Unknown <dylan.gogeta@gmail.com>
2018-12-01 12:20:28 +0100

    Class Arme de Jet
    Donne la Puissance du tir
    Message de préparation du tir
    Selectionne l'object à lancer

---
+++ KtaPulte/Class/ArmeDeJet.cs
@@ -0,0 +1,26 @@
+﻿using System;
+namespace KtaPulte.Class
+{
+    public abstract class ArmeDeJet: CorpsCatapulte
+    {
+        public void Tirer(int num)
+        {
+            Console.WriteLine("Tirer avec une puissance de " + num);
+        }
+        public void Tirer(string num)
+        {
+            Console.WriteLine("Tirer avec une puissance de " + num);
+        }
+        public int Life { get; set; }
+        public abstract void Tir();
+
+        public virtual void PrepaTir()
+        {
+            Console.WriteLine("Tendre les cordages");
+        }
+        public void Fire()
+        {
+            Console.WriteLine("Tirer oui, mais avec quoi?");
+        }
+    }
+}
