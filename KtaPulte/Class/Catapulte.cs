5bd0af06a6944fae81c3cc78e6cc9786b636f539
Unknown <dylan.gogeta@gmail.com>
2018-12-01 12:24:55 +0100

    Class Catapulte
    Assemblage méthode qui initialise les points de vie de la catapulte

---
+++ KtaPulte/Class/Catapulte.cs
@@ -0,0 +1,31 @@
+﻿using System;
+namespace KtaPulte.Class
+{
+    class Catapulte : ArmeDeJet
+    {
+        //Innitialisation de la catapulte et de touts ses modules
+        public void Assemblage()
+        {
+            //setLifeBody();
+            setLifeSpoon();
+            setLifeRope();
+            setLifeBeam();
+        }
+
+        public override void Tir()
+        {
+            Console.WriteLine("Mettre le feu au poudre");
+        }
+        public override void PrepaTir()
+        {
+            Console.WriteLine("Charger le boulet");
+        }
+        public new void Fire()
+        {
+            Console.WriteLine("Appel de la fonction mère");
+            base.Fire();
+            Console.WriteLine("Feu de tout bois.");
+        }
+
+    }
+}
