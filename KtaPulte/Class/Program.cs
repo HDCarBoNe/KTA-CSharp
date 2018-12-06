ea30bdfb7b6db30deb1ca55dfc15bf280170d899
Antoine Mazoyer <35558783+AntoineMazoyer@users.noreply.github.com>
2018-12-06 16:06:16 +0100

    Voila normalement ça devrait être bon
    Je t'envoie mon program.cs également

---
+++ KtaPulte/Class/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.IO;
+
+namespace ProjetCatapulte
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            ListeObjets<string, int>[] tableau = new ListeObjets<string, int>[5];
+            string[] tab2 = { "Rocher", "Parpin", "Rocher de feu", "Rocher de glace", "Tacos" };
+            int[] tab3 = { 0, 16, 19, 40, 25 };
+            for (int i = 0; i < 5; i++)
+            {
+                tableau[i] = new ListeObjets<string, int>(tab2[i], tab3[i]);
+                Console.WriteLine(tableau[i].ToString());
+            }
+            {
+                CorpsCatapulte KTA = new CorpsCatapulte();
+                KTA.ObjetAEnvoyer();
+                KTA.PreparationDuTir();
+                KTA.TendreLaCorde();
+                KTA.Fire();
+                Console.ReadKey();
+            }
+        }
+    }
+}
