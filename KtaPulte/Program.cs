a298d40eb3ef8470c92cd6c99afa1fe40c81ca61
Antoine Mazoyer <antoine.mazoyer@epsi.fr>
2018-12-06 13:31:45 +0100

    A mettre en plus
    ListeObjet in comming

--- KtaPulte/Program.cs
+++ KtaPulte/Program.cs
@@ -12,22 +12,21 @@ namespace KtaPulte
     {
         static void Main(string[] args)
         {
+            Random aleatoire = new Random();
+            int nbal = aleatoire.Next(1, 41);
             ListeObjets<string, int>[] tableau = new ListeObjets<string, int>[5];
+            int[] tab = new int[5];
+            for (int h = 0; h < 5; h++)
+            {
+                tab[h] = nbal++;
+            }
             string[] tab2 = { "Rocher", "Parpin", "Rocher de feu", "Rocher de glace", "Tacos" };
-            int[] tab3 = { 0, 16, 19, 40, 25 };
+            int[] tab3 = { tab[0], tab[1], tab[2], tab[3], tab[4] };
             for (int i = 0; i < 5; i++)
             {
                 tableau[i] = new ListeObjets<string, int>(tab2[i], tab3[i]);
                 Console.WriteLine(tableau[i].ToString());
             }
-            Catapulte KTA = new Catapulte();
-            KTA.Assemblage();
-            KTA.Tirer(45);
-            string pierre = "Une pierre de 5 Kg";
-            KTA.Tirer(pierre);
-            KTA.getLifeSpoon();
-            KTA.getLifeSpoon();
-            KTA.setDamageBody(6);
             Console.ReadKey();
         }
     }
